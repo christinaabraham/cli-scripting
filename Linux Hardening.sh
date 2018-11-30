beff4068507ffd29e2a739641b866703a27b3881
christi p <40750751+itschristi@users.noreply.github.com>
2018-11-30 15:53:45 -0600

    Updated SSH, Kernel Security features
    TODO: 
    - Disable IP Forwarding
    - Disable Send Packet Redirects 
    - Disable ICMP Redirect Acceptance
    - Enable Bad Error Message Protection
    - Enforce Password Policies 
    - Enable Firewall & Zones

--- Linux Hardening.sh
+++ Linux Hardening.sh
@@ -50,3 +50,18 @@ read -p "Press [Enter] key to continue..."
 echo [TODO] Enforce Password length of 8:  
 echo Add “minlen=8” to the end of the line that has “pam_unix.so” in it
 read -p "Press [Enter] key to continue..."
+
+# Disable SSH or set permissions to which only root users may access its contents.
+echo SETTING SSH PERMISSIONS...
+chown root:root /etc/ssh/sshd_config
+chmod 600 /etc/ssh/sshd_config
+
+# Enable SELinux
+echo ENABLING KERNEL SECURITY MECHANISM...
+echo [TODO] Using a text editor, open the following file:
+nano /etc/selinux/config
+echo [TODO] Make sure the following is enforced: SELINUX=enforcing
+
+# View Firewall configurations
+echo CURRENT FIREWALL STATUS...
+iptables -L
