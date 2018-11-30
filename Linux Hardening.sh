aae8a4548d28674feb161383e57b25754c73638e
christi p <40750751+itschristi@users.noreply.github.com>
2018-11-30 13:10:31 -0600

    Update Linux Hardening.sh

--- Linux Hardening.sh
+++ Linux Hardening.sh
@@ -33,16 +33,16 @@ read -p "Press [Enter] key to continue..."
 echo OPENING PAM PASSWORD FILE...
 gedit /etc/pam.d/common‐password
 
-echo Enforce a password history:  
+echo E[TODO]nforce a password history:  
 echo Add “remember=5” to the end of the line that has “pam_unix.so” in it.
 read -p "Press [Enter] key to continue..."
 
-echo Enforce password complexity with one of each type of character:
+echo [TODO] Enforce password complexity with one of each type of character:
 echo Add “ucredit=‐1 lcredit=‐1 dcredit=‐1 ocredit=‐1” to the end of the line with “pam_cracklib.so” in it.
 echo | Uppercase: ucredit| Lowercase: lcredit | Number: dcredit | Symbol: ocredit | 
 echo Cracklibmay needs to be installed before enforcing password complexity.
 read -p "Press [Enter] key to continue..."
 
-echo Enforce Password length of 8:  
+echo [TODO] Enforce Password length of 8:  
 echo Add “minlen=8” to the end of the line that has “pam_unix.so” in it
 read -p "Press [Enter] key to continue..."
