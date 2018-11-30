cb36ad76483148688d0f5ce69d40e50ddb7a3880
christi p <40750751+itschristi@users.noreply.github.com>
2018-11-30 11:38:52 -0600

    Update Windows Hardening.bat

--- Windows Hardening.bat
+++ Windows Hardening.bat
@@ -4,7 +4,7 @@ echo ENABLING FIREWALL...
 netsh advfirewall set domainprofile state on
 
 
-
+// misc. commands: https://www.acpsd.net/site/handlers/filedownload.ashx?moduleinstanceid=53976&dataid=56014&FileName=00%20-%20CyberPatriot%20Notes%20v4%20-%202013-11-09.pdf
 // server hardening: https://www.upguard.com/blog/the-windows-server-hardening-checklist
 // windows manual hardening: https://www.hardenwindows10forsecurity.com/
 
