ab321488e9d686b872a18111dd8475ca9d28b06d
christi p <40750751+itschristi@users.noreply.github.com>
2018-11-30 15:57:56 -0600

    Update Windows Hardening.bat

--- Windows Hardening.bat
+++ Windows Hardening.bat
@@ -18,8 +18,3 @@ del /S *.mp3
 del /S *.mp4
 del /S *.msc
 del /S *.msi
-
-// misc. commands: https://www.acpsd.net/site/handlers/filedownload.ashx?moduleinstanceid=53976&dataid=56014&FileName=00%20-%20CyberPatriot%20Notes%20v4%20-%202013-11-09.pdf
-// server hardening: https://www.upguard.com/blog/the-windows-server-hardening-checklist
-// windows manual hardening: https://www.hardenwindows10forsecurity.com/
-
