7cf78e8aef765d4fd372e7f9b9dd30d85ac18b8f
christi p <40750751+itschristi@users.noreply.github.com>
2018-11-30 12:57:24 -0600

    Update Linux Hardening.sh

--- Linux Hardening.sh
+++ Linux Hardening.sh
@@ -17,10 +17,26 @@ cat /etc/passwd
 cat /etc/group
 
 echo SETTING AUDIT POLICIES...
+# Install auditing program
 apt‐get install auditd
+# Enable audits
 auditctl –e 1
+# View & modify policies
 gedit/etc/audit/auditd.conf
 
-echo Pause to view and modify policies.
+# Pause to view and modify policies.
 read -p "Press [Enter] key to continue..."
 
+echo OPENING PAM PASSWORD FILE...
+gedit /etc/pam.d/common‐password
+
+# Enforce a password history:  
+## Add “remember=5” to the end of the line that has “pam_unix.so” in it.
+
+# Enforce password complexity with one of each type of character:
+## Add “ucredit=‐1 lcredit=‐1 dcredit=‐1 ocredit=‐1” to the end of the line with “pam_cracklib.so” in it.
+### Uppercase: ucredit, Lowercase: lcredit, Number: dcredit, Symbol: ocredit
+### Cracklibmay needs to be installed before enforcing password complexity
+
+# Enforce Password length of 8:  
+## Add “minlen=8” to the end of the line that has “pam_unix.so” in it
