b514615f10f923ff6e2c10651f8ed6f7097c4f25
christi p <40750751+itschristi@users.noreply.github.com>
2018-11-30 12:20:30 -0600

    Update Windows Hardening.bat

--- Windows Hardening.bat
+++ Windows Hardening.bat
@@ -1,8 +1,23 @@
 @echo off
 title Windows Hardening
+
+REM Enables Windows Firewall for all profiles.
 echo ENABLING FIREWALL...
 netsh advfirewall set domainprofile state on
+netsh advfirewall set domainprofile state on 
+netsh advfirewall set privateprofile state on
 
+REM Checks for media files and malware.
+echo DELETING MEDIA...
+cd C:\
+del /S *.cpl
+del /S *.dll
+del /S *.exe
+del /S *.png
+del /S *.mp3
+del /S *.mp4
+del /S *.msc
+del /S *.msi
 
 // misc. commands: https://www.acpsd.net/site/handlers/filedownload.ashx?moduleinstanceid=53976&dataid=56014&FileName=00%20-%20CyberPatriot%20Notes%20v4%20-%202013-11-09.pdf
 // server hardening: https://www.upguard.com/blog/the-windows-server-hardening-checklist
